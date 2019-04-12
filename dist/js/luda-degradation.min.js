b267beadcae10defd4a4b14ae137c5dc3618bdf1
littlstonee <littlstonee@hotmail.com>
2019-04-12 17:23:10 +0800

    rebuild

--- dist/js/luda-degradation.min.js
+++ dist/js/luda-degradation.min.js
@@ -1,5 +1,5 @@
 /*! 
- * Luda degradation script 0.1.12 | https://luda.dev
+ * Luda degradation script 0.1.13 | https://luda.dev
  * Copyright 2018 oatw | https://oatw.blog
  * MIT license | http://opensource.org/licenses/MIT
  */
